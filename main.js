a6404ce8ae2ef5135c419839ebc95883029e9116
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-06-14 10:12:10 +0100

    Add js-functionality to hide-overlay

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const menu = document.querySelector('.hamburger');
 const overlay = document.querySelector('.overlay');
+const close = document.querySelector('.close-icon');
 
 
 menu.addEventListener('click', ()=>{
-    overlay.style.display ="block"
+    overlay.style.display ="block"    
+})
+
+close.addEventListener('click', ()=>{
+    overlay.style.display ="none"    
 })
