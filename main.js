386304b0a653e6aa1f4fb64f6f16e608e65578de
Alphy Gacheru <alphygacheru@gmail.com>
2022-06-14 11:41:21 +0300

    Add js logic to show overlay when hamburger menu is clicked

---
+++ main.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('.hamburger');
+const overlay = document.querySelector('.overlay');
+
+
+menu.addEventListener('click', ()=>{
+    overlay.style.display ="block"
+})
