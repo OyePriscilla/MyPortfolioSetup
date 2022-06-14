361597c1719c2687a7ae9eccacc395f3d1d14184
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-06-14 12:35:28 +0100

    Add eslint linter file

--- main.js
+++ main.js
@@ -2,26 +2,26 @@ const menu = document.querySelector('.hamburger');
 const overlay = document.querySelector('.overlay');
 const close = document.querySelector('.close-icon');
 
-const overlayLinkOne = document.querySelector('.overlay-link-one')
-const overlayLinkTwo = document.querySelector('.overlay-link-two')
-const overlayLinkThree = document.querySelector('.overlay-link-three')
+const overlayLinkOne = document.querySelector('.overlay-link-one');
+const overlayLinkTwo = document.querySelector('.overlay-link-two');
+const overlayLinkThree = document.querySelector('.overlay-link-three');
 
-menu.addEventListener('click', ()=>{
-    overlay.style.display ="block"    
-})
+menu.addEventListener('click', () => {
+  overlay.style.display = 'block';
+});
 
-close.addEventListener('click', ()=>{
-    overlay.style.display ="none"    
-})
+close.addEventListener('click', () => {
+  overlay.style.display = 'none';
+});
 
-overlayLinkOne.addEventListener('click', ()=>{
-    overlay.style.display ="none"
-})
+overlayLinkOne.addEventListener('click', () => {
+  overlay.style.display = 'none';
+});
 
-overlayLinkTwo.addEventListener('click', ()=>{
-    overlay.style.display ="none"
-})
+overlayLinkTwo.addEventListener('click', () => {
+  overlay.style.display = 'none';
+});
 
-overlayLinkThree.addEventListener('click', ()=>{
-    overlay.style.display ="none"
-})
+overlayLinkThree.addEventListener('click', () => {
+  overlay.style.display = 'none';
+});
