5a7ce00d4b27ec5aee8357429315665af5e6c82c
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-06-17 17:46:51 +0100

    Linters check

--- main.js
+++ main.js
@@ -1,125 +1,125 @@
-const menu = document.querySelector(".hamburger");
-const overlay = document.querySelector(".overlay");
-const close = document.querySelector(".close-icon");
+const menu = document.querySelector('.hamburger');
+const overlay = document.querySelector('.overlay');
+const close = document.querySelector('.close-icon');
 
-const overlayLinkOne = document.querySelector(".overlay-link-one");
-const overlayLinkTwo = document.querySelector(".overlay-link-two");
-const overlayLinkThree = document.querySelector(".overlay-link-three");
-const desktopWorkSection = document.getElementById("desktop-works");
+const overlayLinkOne = document.querySelector('.overlay-link-one');
+const overlayLinkTwo = document.querySelector('.overlay-link-two');
+const overlayLinkThree = document.querySelector('.overlay-link-three');
+const desktopWorkSection = document.getElementById('desktop-works');
 
-const mobileModalButtonOne = document.querySelector("#bttn");
-const mobileModalButtonTwo = document.querySelector(".btn-touch-two");
-const mobileModalButtonThree = document.querySelector(".btn-touch-three");
-const mobileModalButtonFour = document.querySelector(".btn-touch-four");
-const closeMobileButton = document.querySelector(".close-mobile-button");
+const mobileModalButtonOne = document.querySelector('#bttn');
+const mobileModalButtonTwo = document.querySelector('.btn-touch-two');
+const mobileModalButtonThree = document.querySelector('.btn-touch-three');
+const mobileModalButtonFour = document.querySelector('.btn-touch-four');
+const closeMobileButton = document.querySelector('.close-mobile-button');
 
-mobileModalButtonOne.addEventListener("click", () => {
-  document.querySelector(".desktop-mode").style.display = "none";
-  document.querySelector(".mobile-modal-container").style.display = "block";
+mobileModalButtonOne.addEventListener('click', () => {
+  document.querySelector('.desktop-mode').style.display = 'none';
+  document.querySelector('.mobile-modal-container').style.display = 'block';
 });
 
-mobileModalButtonTwo.addEventListener("click", () => {
-  document.querySelector(".desktop-mode").style.display = "none";
-  document.querySelector(".mobile-modal-container").style.display = "block";
+mobileModalButtonTwo.addEventListener('click', () => {
+  document.querySelector('.desktop-mode').style.display = 'none';
+  document.querySelector('.mobile-modal-container').style.display = 'block';
 });
 
-mobileModalButtonThree.addEventListener("click", () => {
-  document.querySelector(".desktop-mode").style.display = "none";
-  document.querySelector(".mobile-modal-container").style.display = "block";
+mobileModalButtonThree.addEventListener('click', () => {
+  document.querySelector('.desktop-mode').style.display = 'none';
+  document.querySelector('.mobile-modal-container').style.display = 'block';
 });
 
-mobileModalButtonFour.addEventListener("click", () => {
-  document.querySelector(".desktop-mode").style.display = "none";
-  document.querySelector(".mobile-modal-container").style.display = "block";
+mobileModalButtonFour.addEventListener('click', () => {
+  document.querySelector('.desktop-mode').style.display = 'none';
+  document.querySelector('.mobile-modal-container').style.display = 'block';
 });
 
-closeMobileButton.addEventListener("click", () => {
-  document.querySelector(".desktop-mode").style.display = "block";
-  document.querySelector(".mobile-modal-container").style.display = "none";
+closeMobileButton.addEventListener('click', () => {
+  document.querySelector('.desktop-mode').style.display = 'block';
+  document.querySelector('.mobile-modal-container').style.display = 'none';
 });
 
 document
-  .querySelector("#btn-submit-form")
-  .addEventListener("click", (event) => {
-    const email = document.getElementById("email-contact").value;
-    const error = document.querySelector(".error-email-class");
+  .querySelector('#btn-submit-form')
+  .addEventListener('click', (event) => {
+    const email = document.getElementById('email-contact').value;
+    const error = document.querySelector('.error-email-class');
     const isLowerCase = (str) => str === str.toLowerCase();
     if (!isLowerCase(email)) {
       event.preventDefault();
-      error.style.display = "block";
+      error.style.display = 'block';
       setTimeout(() => {
-        error.style.display = "none";
+        error.style.display = 'none';
       }, 5000);
     }
   });
 
-menu.addEventListener("click", () => {
-  overlay.style.display = "block";
+menu.addEventListener('click', () => {
+  overlay.style.display = 'block';
 });
 
-close.addEventListener("click", () => {
-  overlay.style.display = "none";
+close.addEventListener('click', () => {
+  overlay.style.display = 'none';
 });
 
-overlayLinkOne.addEventListener("click", () => {
-  overlay.style.display = "none";
+overlayLinkOne.addEventListener('click', () => {
+  overlay.style.display = 'none';
 });
 
-overlayLinkTwo.addEventListener("click", () => {
-  overlay.style.display = "none";
+overlayLinkTwo.addEventListener('click', () => {
+  overlay.style.display = 'none';
 });
 
-overlayLinkThree.addEventListener("click", () => {
-  overlay.style.display = "none";
+overlayLinkThree.addEventListener('click', () => {
+  overlay.style.display = 'none';
 });
 
 // Portfolio projects in an array
 const projects = [
   {
     id: 1,
-    name: "Tonic",
+    name: 'Tonic',
     description:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    featured_img: "./images/desktop-works/work-one.png",
-    tecnologies: ["html", "css", "javascript"],
-    live_link: "https://alphayowakarindi.github.io/My-Portfolio-1/",
-    source_link: "https://github.com/alphayowakarindi/My-Portfolio-1",
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featured_img: './images/desktop-works/work-one.png',
+    tecnologies: ['html', 'css', 'javascript'],
+    live_link: 'https://alphayowakarindi.github.io/My-Portfolio-1/',
+    source_link: 'https://github.com/alphayowakarindi/My-Portfolio-1',
   },
   {
     id: 2,
-    name: "Multi-Post Stories",
+    name: 'Multi-Post Stories',
     description:
-      "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
-    featured_img: "./images/desktop-works/work-two.png",
-    tecnologies: ["html", "Ruby on rails", "css", "javascript"],
-    live_link: "https://alphayowakarindi.github.io/My-Portfolio-2/",
-    source_link: "https://github.com/alphayowakarindi/My-Portfolio-2",
+      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    featured_img: './images/desktop-works/work-two.png',
+    tecnologies: ['html', 'Ruby on rails', 'css', 'javascript'],
+    live_link: 'https://alphayowakarindi.github.io/My-Portfolio-2/',
+    source_link: 'https://github.com/alphayowakarindi/My-Portfolio-2',
   },
   {
     id: 3,
-    name: "Facebook 360",
+    name: 'Facebook 360',
     description:
-      "Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
-    featured_img: "./images/desktop-works/work-three.png",
-    tecnologies: ["html", "Ruby on rails", "css", "javascript"],
-    live_link: "https://alphayowakarindi.github.io/My-Portfolio-3/",
-    source_link: "https://github.com/alphayowakarindi/My-Portfolio-3",
+      'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+    featured_img: './images/desktop-works/work-three.png',
+    tecnologies: ['html', 'Ruby on rails', 'css', 'javascript'],
+    live_link: 'https://alphayowakarindi.github.io/My-Portfolio-3/',
+    source_link: 'https://github.com/alphayowakarindi/My-Portfolio-3',
   },
   {
     id: 4,
-    name: "Uber Navigation",
+    name: 'Uber Navigation',
     description:
-      "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
-    featured_img: "./images/desktop-works/work-four.png",
-    tecnologies: ["html", "Ruby on rails", "css", "javascript"],
-    live_link: "https://alphayowakarindi.github.io/My-Portfolio-4/",
-    source_link: "https://github.com/alphayowakarindi/My-Portfolio-4",
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    featured_img: './images/desktop-works/work-four.png',
+    tecnologies: ['html', 'Ruby on rails', 'css', 'javascript'],
+    live_link: 'https://alphayowakarindi.github.io/My-Portfolio-4/',
+    source_link: 'https://github.com/alphayowakarindi/My-Portfolio-4',
   },
 ];
 
 //
-window.addEventListener("DOMContentLoaded", () => {
-  let projectsInHtmlVersion = "";
+window.addEventListener('DOMContentLoaded', () => {
+  let projectsInHtmlVersion = '';
   for (let i = 0; i < projects.length; i += 1) {
     projectsInHtmlVersion += ` 
                 <div class="work">
@@ -139,12 +139,12 @@ window.addEventListener("DOMContentLoaded", () => {
                   </p>
                   <ul>
                     ${projects[i].tecnologies
-                      .map((tech) => `<li>${tech}</li>`)
-                      .join("")}
+    .map((tech) => `<li>${tech}</li>`)
+    .join('')}
                   </ul>
                   <button class='pop-desktop-modal-btn' type="submit"  data-id=${
-                    projects[i].id
-                  }>See Project</button>
+  projects[i].id
+}>See Project</button>
                 </div>
               </div>`;
   }
@@ -153,34 +153,34 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 // event delegation will allow us to access any JS dynamic object
-document.addEventListener("click", (e) => {
-  if (e.target.className === "pop-desktop-modal-btn") {
-    document.querySelector(".desktop-modal-container").style.display = "block";
+document.addEventListener('click', (e) => {
+  if (e.target.className === 'pop-desktop-modal-btn') {
+    document.querySelector('.desktop-modal-container').style.display = 'block';
     return;
   }
-  if (e.target.className === "close-desktop-modal") {
-    document.querySelector(".desktop-modal-container").style.display = "none";
+  if (e.target.className === 'close-desktop-modal') {
+    document.querySelector('.desktop-modal-container').style.display = 'none';
   }
 });
 
 // Local storage
-const contactForm = document.getElementById("form");
-const [userName, email, message] = form.elements;
+const contactForm = document.getElementById('form');
+const [userName, email, message] = contactForm.elements;
 
-if (!localStorage.getItem("form-detail")) {
+if (!localStorage.getItem('form-detail')) {
   const data = {
-    nameContent: "",
-    emailContent: "",
-    messageContent: "",
+    nameContent: '',
+    emailContent: '',
+    messageContent: '',
   };
-  localStorage.setItem("form-detail", JSON.stringify(data));
+  localStorage.setItem('form-detail', JSON.stringify(data));
 }
 
 const setBrowserData = (element, elementValue) => {
-  element.addEventListener("change", () => {
-    const retrivedData = JSON.parse(localStorage.getItem("form-detail"));
+  element.addEventListener('change', () => {
+    const retrivedData = JSON.parse(localStorage.getItem('form-detail'));
     retrivedData[elementValue] = element.value;
-    localStorage.setItem("form-detail", JSON.stringify(retrivedData));
+    localStorage.setItem('form-detail', JSON.stringify(retrivedData));
   });
 };
 
@@ -188,11 +188,11 @@ const setFormDetails = (element, elementValue) => {
   element.value = elementValue;
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  setBrowserData(userName, "nameContent");
-  setBrowserData(email, "emailContent");
-  setBrowserData(message, "messageContent");
-  const data = JSON.parse(localStorage.getItem("form-detail"));
+document.addEventListener('DOMContentLoaded', () => {
+  setBrowserData(userName, 'nameContent');
+  setBrowserData(email, 'emailContent');
+  setBrowserData(message, 'messageContent');
+  const data = JSON.parse(localStorage.getItem('form-detail'));
   setFormDetails(userName, data.nameContent);
   setFormDetails(email, data.emailContent);
   setFormDetails(message, data.messageContent);
